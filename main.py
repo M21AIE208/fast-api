08217d87411e09db2377b9d2f1b37e0e104a11a6
ajaykumar71292 <ajay.kumar@bain.com>
2025-05-25 12:08:49 +0530

    feat: adding post method

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = Flask(__name__) # Initialize Flask app
 def home():
     return "Welcome to the Ajay's Flask App!"
 
+@app.route("/get_sqaure", methods=["POST"])
+def get_square():
+    data = request.get_json()
+    number = data.get("number")
+    return jsonify({"square": number ** 2})
+
 
 
 if __name__ == "__main__":
